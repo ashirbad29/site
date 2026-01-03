f89ec263c88f5cce5e9100816f6a915678527ed2
Ashirbad <ashirbad.behera@gocomet.com>
2026-01-04 01:32:07 +0530

    Add fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['Inter', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'sans-serif'],
+				heading: ['Cabinet Grotesk', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 }
