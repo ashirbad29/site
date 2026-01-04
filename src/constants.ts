ef2b813e3417ed5291debf4e8bc5dcec574225dd
Ashirbad <ashirbad.behera@gocomet.com>
2026-01-04 18:40:15 +0530

    formatting and link component

--- src/constants.ts
+++ src/constants.ts
@@ -1,22 +1,25 @@
-export const HOME_TITLE = "Ashirbad – Home"
-export const HOME_DESCRIPTION = "Personal website of Ashirbad Behera, a software engineer"
+export const HOME_TITLE = 'Ashirbad – Home';
+export const HOME_DESCRIPTION =
+	'Personal website of Ashirbad, a software engineer';
 
 export const NAV_ITEMS = [
-  { label: "Index", href: "/" },
-  // { label: "About", href: "#" },
-  { label: "Blog", href: "#" },
-  { label: "Work", href: "#" },
-  // { label: "Now", href: "#" },
-]
+	{ label: 'Index', href: '/' },
+	// { label: "About", href: "#" },
+	{ label: 'Blog', href: '#' },
+	{ label: 'Work', href: '#' },
+	// { label: "Now", href: "#" },
+];
 
-export const SOCIAL_LINKS = [
-  { label: "GitHub", href: "#", icon: "code" },
-  { label: "RSS Feed", href: "#", icon: "rss" },
-  // { label: "Twitter", href: "#", icon: "twitter" },
-]
+export const SOCIALS = {
+	github: 'https://github.com/ashirbad29',
+	twitter: 'https://twitter.com/ashirbad_29',
+	linkedin: 'https://www.linkedin.com/in/ashirbad29/',
+	email: 'ashirbadbehera29@gmail.com',
+	aniList: 'https://anilist.co/user/ashirbad29/animelist',
+};
 
 export const RECENT_POSTS = [
-  { title: "Getting started with Astro", date: "Dec 2024" },
-  { title: "Building a personal blog", date: "Nov 2024" },
-  { title: "Minimal design principles", date: "Oct 2024" },
-]
+	{ title: 'Getting started with Astro', date: 'Dec 2024' },
+	{ title: 'Building a personal blog', date: 'Nov 2024' },
+	{ title: 'Minimal design principles', date: 'Oct 2024' },
+];
